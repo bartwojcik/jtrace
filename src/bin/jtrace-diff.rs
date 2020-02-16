4d46730407017bf89c176ab96aeae1fe44ff79d6
Bartosz Wojcik <bartwojc@gmail.com>
2020-02-16 23:56:45 +0100

    Add some (failing) tests

--- src/bin/jtrace-diff.rs
+++ src/bin/jtrace-diff.rs
@@ -118,6 +118,7 @@ fn mod_idx(n: i64, m: i64) -> usize {
     (((n % m) + m) % m) as usize
 }
 
+#[derive(Debug)]
 enum DiffOperation {
     Deletion { pos_old: usize },
     Insertion { pos_old: usize, pos_new: usize },
@@ -125,82 +126,145 @@ enum DiffOperation {
 
 type DiffResults = Vec<DiffOperation>;
 
-/// Ported to Rust from this Python code:
+// TODO possibly move to different module
+/// Diff/LCS algorithm from E. Myers paper "An O(ND) Difference Algorithm and Its Variations":
+/// http://www.xmailserver.org/diff2.pdf
+/// Includes optimizations from:
 /// https://github.com/RobertElderSoftware/roberteldersoftwarediff/blob/master/myers_diff_and_variations.py
 fn diff<T: Eq + Clone>(left_sequence: &[T], right_sequence: &[T],
-                       i: Option<usize>, j: Option<usize>, results: &mut DiffResults) {
+                       i: Option<usize>, j: Option<usize>,
+                       v_f: &mut [i64], v_b: &mut [i64],
+                       results: &mut DiffResults) {
     let i = i.unwrap_or(0);
     let j = j.unwrap_or(0);
     let l_len = left_sequence.len() as i64;
     let r_len = right_sequence.len() as i64;
-    let sum_l = l_len + r_len;
+    let sum_len = l_len + r_len;
     let v_size = 2 * min(l_len, r_len) + 2;
+    // use a smaller slice into the same shared buffers
+    let v_f = &mut v_f[0..v_size as usize];
+    let v_b = &mut v_b[0..v_size as usize];
     if l_len > 0 && r_len > 0 {
         let delta = l_len - r_len;
-        let mut v_forward = vec![0; v_size as usize];
-        let mut v_backward = vec![0; v_size as usize];
-        for h in 0..sum_l / 2 + sum_l % 2 + 1 {
-            for &forward in &[true, false] {
-                let (v_a, v_b, p, q);
-                if forward {
-                    v_a = &mut v_forward;
-                    v_b = &v_backward;
-                    p = 1;
-                    q = 1;
+        // zero out the (shared) v arrays
+        v_f.iter_mut().for_each(|x| *x = 0);
+        v_b.iter_mut().for_each(|x| *x = 0);
+        // TODO ceil?
+        for h in 0..sum_len / 2 + sum_len % 2 + 1 {
+            // forward (step by 2)
+            let mut k = -(h - 2 * max(0, h - r_len));
+            while k <= h - 2 * max(0, h - l_len) {
+                // indices into v array:
+                let c_idx = mod_idx(k, v_size); // current k line
+                let a_idx = mod_idx(k + 1, v_size); // k line above
+                let b_idx = mod_idx(k - 1, v_size); // k line below
+                // select the better incoming path
+                let mut a = if k == -h || k != h && v_f[b_idx] < v_f[a_idx] {
+                    v_f[a_idx]
                 } else {
-                    v_a = &mut v_backward;
-                    v_b = &v_forward;
-                    p = 0;
-                    q = -1;
+                    v_f[b_idx] + 1
+                };
+                // calculate y coordinate from k and x
+                let mut b = a - k;
+                // save the coordinates of the snake's starting point
+                let (s, t) = (a, b);
+                // move diagonally as much as possible
+                while a < l_len && b < r_len
+                    && left_sequence[a as usize] == right_sequence[b as usize] {
+                    a += 1;
+                    b += 1;
                 }
-                let mut k = -(h - 2 * max(0, h - r_len));
-                while k <= h - 2 * max(0, h - l_len) {
-                    // select better incoming path
-                    let c_idx = mod_idx(k, v_size); // current k line
-                    let a_idx = mod_idx(k + 1, v_size); // k line above
-                    let b_idx = mod_idx(k - 1, v_size); // k line below
-                    let mut a = if k == -h || k != h && v_a[b_idx] < v_a[a_idx] {
-                        v_a[a_idx]
-                    } else {
-                        v_a[b_idx] + 1
-                    };
-                    let mut b = a - k;
-                    // save the coordinates of the snake's starting point
-                    let (s, t) = (a, b);
-                    // follow the diagonals
-                    while a < l_len && b < r_len
-                        && left_sequence[((1 - p) * l_len + q * a + (p - 1)) as usize]
-                        == right_sequence[((1 - p) * r_len + q * b + (p - 1)) as usize] {
-                        a += 1;
-                        b += 1;
+                // update the "best x" array
+                v_f[c_idx] = a;
+                // v array index for current inverse k line
+                let inverse_k = -k + delta;
+                let inv_c_idx = mod_idx(inverse_k, v_size);
+                // check for path overlap
+                if sum_len % 2 == 1 && inverse_k >= -h + 1 && inverse_k <= h - 1
+                    && v_f[c_idx] + v_b[inv_c_idx] >= l_len {
+                    let (l_len, r_len) = (l_len as usize, r_len as usize);
+                    let (d, x, y, u, v) = (2 * h - 1,
+                                           s as usize, t as usize,
+                                           a as usize, b as usize);
+                    if d > 1 || x != u && y != u {
+                        diff(&left_sequence[0..x], &right_sequence[0..y],
+                             Some(i), Some(j),
+                             v_f, v_b,
+                             results);
+                        diff(&left_sequence[u..l_len], &right_sequence[v..r_len],
+                             Some(i + u), Some(j + v),
+                             v_f, v_b,
+                             results);
+                        return;
+                    } else if r_len > l_len {
+                        diff(&[], &right_sequence[l_len..r_len],
+                             Some(i + l_len), Some(j + l_len),
+                             v_f, v_b,
+                             results);
+                        return;
+                    } else if r_len < l_len {
+                        diff(&left_sequence[r_len..l_len], &[],
+                             Some(i + r_len), Some(j + r_len),
+                             v_f, v_b,
+                             results);
+                        return;
                     }
-                    // update best x array
-                    v_a[c_idx] = a;
-                    let inverse_k = -(k - delta);
-                    if sum_l % 2 == p && inverse_k >= -(h - p) && inverse_k <= h - p
-                        && v_a[c_idx] + v_b[c_idx] >= l_len {
-                        let (l_len, r_len) = (l_len as usize, r_len as usize);
-                        let (d, x, y, u, v) = if forward {
-                            (2 * h - 1, s as usize, t as usize, a as usize, b as usize)
-                        } else {
-                            (2 * h, l_len - a as usize, r_len - b as usize,
-                             l_len - s as usize, r_len - t as usize)
-                        };
-                        if d > 1 || x != u && y != u {
-                            diff(&left_sequence[0..x], &right_sequence[0..y],
-                                 Some(i), Some(j), results);
-                            diff(&left_sequence[u..l_len], &right_sequence[v..r_len],
-                                 Some(i + u), Some(j + v), results);
-                        } else if r_len > l_len {
-                            diff(&[], &right_sequence[l_len..r_len],
-                                 Some(i + l_len), Some(j + l_len), results);
-                        } else if r_len < l_len {
-                            diff(&left_sequence[r_len..l_len], &[],
-                                 Some(i + r_len), Some(j + r_len), results);
-                        }
-                    }
-                    k += 2;
                 }
+                k += 2;
+            }
+            // backward
+            k = -(h - 2 * max(0, h - r_len));
+            while k <= h - 2 * max(0, h - l_len) {
+                let c_idx = mod_idx(k, v_size);
+                let a_idx = mod_idx(k + 1, v_size);
+                let b_idx = mod_idx(k - 1, v_size);
+                let mut a = if k == -h || k != h && v_b[b_idx] < v_b[a_idx] {
+                    v_b[a_idx]
+                } else {
+                    v_b[b_idx] + 1
+                };
+                let mut b = a - k;
+                let (s, t) = (a, b);
+                while a < l_len && b < r_len
+                    && left_sequence[(l_len - a - 1) as usize]
+                    == right_sequence[(r_len - b - 1) as usize] {
+                    a += 1;
+                    b += 1;
+                }
+                v_b[c_idx] = a;
+                let inverse_k = -k + delta;
+                let inv_c_idx = mod_idx(inverse_k, v_size);
+                if sum_len % 2 == 0 && inverse_k >= -h && inverse_k <= h
+                    && v_b[c_idx] + v_f[inv_c_idx] >= l_len {
+                    let (l_len, r_len) = (l_len as usize, r_len as usize);
+                    let (d, x, y, u, v) = (2 * h,
+                                           l_len - a as usize, r_len - b as usize,
+                                           l_len - s as usize, r_len - t as usize);
+                    if d > 1 || x != u && y != u {
+                        diff(&left_sequence[0..x], &right_sequence[0..y],
+                             Some(i), Some(j),
+                             v_f, v_b,
+                             results);
+                        diff(&left_sequence[u..l_len], &right_sequence[v..r_len],
+                             Some(i + u), Some(j + v),
+                             v_f, v_b,
+                             results);
+                        return;
+                    } else if r_len > l_len {
+                        diff(&[], &right_sequence[l_len..r_len],
+                             Some(i + l_len), Some(j + l_len),
+                             v_f, v_b,
+                             results);
+                        return;
+                    } else if r_len < l_len {
+                        diff(&left_sequence[r_len..l_len], &[],
+                             Some(i + r_len), Some(j + r_len),
+                             v_f, v_b,
+                             results);
+                        return;
+                    }
+                }
+                k += 2;
             }
         }
     } else if l_len > 0 {
@@ -214,12 +278,15 @@ fn diff<T: Eq + Clone>(left_sequence: &[T], right_sequence: &[T],
     }
 }
 
-
-fn find_difference<T: Eq + Clone>(left_sequence: &[T],
-                          right_sequence: &[T])
-                   -> DiffResults {
+fn find_difference<T: Eq + Clone>(left_sequence: &[T], right_sequence: &[T]) -> DiffResults {
+    let v_size = 2 * min(left_sequence.len(), right_sequence.len()) + 2;
+    let mut v_forward = vec![0; v_size as usize];
+    let mut v_backward = vec![0; v_size as usize];
     let mut results = DiffResults::new();
-    diff(left_sequence, right_sequence, None, None, &mut results);
+    diff(left_sequence, right_sequence,
+         None, None,
+         &mut v_forward, &mut v_backward,
+         &mut results);
     results
 }
 
@@ -256,6 +323,114 @@ fn main() {
         while let Some(source) = e.source() {
             error!("Caused by: {}", source.to_string());
             e = source;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rand::{Rng, thread_rng};
+    use rand::distributions::Alphanumeric;
+
+    use super::*;
+
+    fn apply_difference<T: Eq + Clone>(left_sequence: &[T], right_sequence: &[T],
+                                       difference: &[DiffOperation]) -> Vec<T> {
+        let mut result = Vec::<T>::new();
+        let mut i = 0;
+        let mut j = 0;
+        while j < difference.len() {
+            let next_pos = match difference[j] {
+                DiffOperation::Deletion { pos_old: x } => x,
+                DiffOperation::Insertion { pos_old: x, pos_new: _ } => x,
+            };
+            if i == next_pos {
+                match difference[j] {
+                    DiffOperation::Deletion { pos_old: _ } => {
+                        i += 1;
+                    }
+                    DiffOperation::Insertion { pos_old: _, pos_new: x } =>
+                        {
+                            result.push(right_sequence[x].clone());
+                        }
+                };
+                j += 1;
+            } else {
+                result.push(left_sequence[i].clone());
+                i += 1;
+            }
+        }
+        while i < left_sequence.len() {
+            result.push(left_sequence[i].clone());
+            i += 1;
+        }
+        result
+    }
+
+    fn test_two_strs(str1: &str, str2: &str, expected_diff_len: usize) {
+        let seq1 = str1.as_bytes();
+        let seq2 = str2.as_bytes();
+        let difference = find_difference(seq1, seq2);
+        let recon_seq2 = apply_difference(seq1, seq2, &difference);
+        let recon_str2 = std::str::from_utf8(&recon_seq2).unwrap();
+        assert!(recon_seq2 == seq2, "seq1: {}, seq2: {}, recon_seq2: {}", str1, str2, recon_str2);
+        let actual = difference.len();
+        assert!(actual == expected_diff_len, "difference len: {}, expected len: {}",
+                actual, expected_diff_len);
+    }
+
+    #[test]
+    fn test_str_from_paper() {
+        test_two_strs("abcabba", "cbabac", 5);
+    }
+
+    #[test]
+    fn test_str_from_paper_2() {
+        test_two_strs("abcabbac", "cbabac", 4);
+    }
+
+    #[test]
+    fn test_evil_case_1() {
+        test_two_strs("nitWVMHDydSTnFbvsSFDTiQfUMFkLTyemjKVqWlTfZOESYyPYaJnpTqMfakxX",
+                      "n8tWVMHDyd6TnFbvsSFDTiQ5UMF0LTy1mjKVqWlTf5OESYyPY8JnpTqMfak3X",
+                      16);
+    }
+
+    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    const DIGITSET: &[u8] = b"0123456789";
+
+    fn random_string(len: usize) -> String {
+        let mut rng = thread_rng();
+        (0..len).map(|_| {
+            let idx = rng.gen_range(0, CHARSET.len());
+            CHARSET[idx] as char
+        }).collect()
+    }
+
+    fn random_perturbation(source: &str, chance: f64) -> (String, usize) {
+        let mut rng = thread_rng();
+        let mut s: String = source.into();
+        let mut modified = 0;
+        s = s.chars().map(|c| {
+            let p: f64 = rng.gen();
+            if p <= chance {
+                modified += 1;
+                let idx = rng.gen_range(0, DIGITSET.len());
+                DIGITSET[idx] as char
+            } else {
+                c
+            }
+        }).collect();
+        (s, modified)
+    }
+
+    #[test]
+    fn test_random_strs() {
+        for i in 0..1000 {
+            let mut rng = thread_rng();
+            let str1 = random_string(rng.gen_range(10, 100));
+            let (str2, modified) = random_perturbation(&str1, rng.gen_range(0.05, 0.5));
+            test_two_strs(&str1, &str2, modified * 2);
         }
     }
 }
